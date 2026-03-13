e397fd8e38b274a57e550f3e3d8053a9e2cde852
Grigoriy Dubrovín <wackywildoak@gmail.com>
2026-03-14 03:35:11 +0900

    feat: подключить di контейнер

---
+++ bootstrap/container.php
@@ -0,0 +1,9 @@
+<?php
+
+use DI\ContainerBuilder;
+
+$builder = new ContainerBuilder();
+
+$builder->addDefinitions(__DIR__ . '../config/di.php');
+
+return $builder->build();
---
+++ config/di.php
@@ -0,0 +1,3 @@
+<?php
+
+use DI\ContainerBuilder;
