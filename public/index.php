c1ca9f6a503cb67900cfe9a56539c3e57aa0ce2b
Grigoriy Dubrovín <wackywildoak@gmail.com>
2026-03-14 03:35:28 +0900

    feat: создать точку входа приложения

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$container = require_once __DIR__ . '/../bootstrap/container.php';
+
+$router = require_once __DIR__ . '/../routes/api.php';
