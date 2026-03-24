35b719dfdb11039bc105f9031348e1964cf05c62
Grigoriy Dubrovín <wackywildoak@gmail.com>
2026-03-25 02:33:11 +0900

    fix: передавать request в конструкторе контроллеров

--- src/Reservation/Application/Service/AuthService.php
+++ src/Reservation/Application/Service/AuthService.php
@@ -6,13 +6,7 @@ namespace App\Reservation\Application\Service;
 
 use App\Reservation\Domain\Entity\User;
 use App\Reservation\Domain\ValueObject\Uuid;
-use App\Reservation\Domain\ValueObject\Email;
-use App\Reservation\Domain\ValueObject\UserRole;
 use App\Reservation\Domain\Repository\UserRepositoryInterface;
-use App\Reservation\Domain\Repository\ClientProfileRepositoryInterface;
-use App\Reservation\Application\DTO\RegisterUserRequest;
-use App\Reservation\Application\DTO\LoginUserRequest;
-use App\Reservation\Domain\Entity\ClientProfile;
 use App\Reservation\Infrastructure\Auth\JwtManager;
 use App\Reservation\Infrastructure\Auth\RefreshTokenStorage;
 use OutOfBoundsException;
@@ -22,61 +16,9 @@ class AuthService
 {
     public function __construct(
         private UserRepositoryInterface $userRepository,
-        private ClientProfileRepositoryInterface $clientProfileRepository,
         private RefreshTokenStorage $tokenStorage,
         private JwtManager $jwtManager
     ) {}
-
-    public function register(
-        RegisterUserRequest $userDTO,
-    ): void
-    {
-        if ($this->userRepository->findByEmail($userDTO->email) !== null) {
-            throw new \Exception('User with this email already exists', 401);
-        }
-
-        if (strlen($userDTO->password) < 6) {
-            throw new \Exception('Password must be at least 6 characters long', 401);
-        }
-
-        if (strlen($userDTO->name) < 2) {
-            throw new \Exception('Name must be at least 2 characters long', 401);
-        }
-
-        if (!filter_var($userDTO->email, FILTER_VALIDATE_EMAIL)) {
-            throw new \Exception('Invalid email address', 401);
-        }
-
-        $user = new User(
-            id: Uuid::generate(),
-            email: new Email($userDTO->email),
-            name: $userDTO->name,
-            passwordHash: password_hash($userDTO->password, PASSWORD_DEFAULT),
-            role: UserRole::Client,
-            createdAt: new \DateTime()
-        );
-
-        switch($user->role) {
-            case UserRole::Client:
-                $clientProfile = ClientProfile::create(
-                    id: Uuid::generate(),
-                    userId: $user->id
-                );
-                $this->clientProfileRepository->save($clientProfile);
-        }
-
-        $this->userRepository->save($user);
-    }
-
-    public function login(LoginUserRequest $userDTO): array
-    {
-        $user = $this->userRepository->findByEmail($userDTO->email);
-        if ($user === null || !password_verify($userDTO->password, $user->passwordHash)) {
-            throw new \Exception('Invalid email or password', 401);
-        }
-
-        return $this->generateTokens($user, Uuid::generate()->value);
-    }
 
     public function logout(string $token): void
     {
@@ -88,13 +30,18 @@ class AuthService
             throw new Exception('Неверный токен', 401);
         } 
 
+        $this->revokeRefreshToken($tokenData['id']);
+    }
+
+    public function revokeRefreshToken(string $tokenId): void
+    {
         $this->tokenStorage->update(
-            id: $tokenData['id'],
+            id: $tokenId,
             isRevoked: true,
         );
     }
 
-    private function generateTokens(User $user, string $familyId): array
+    public function generateTokens(User $user, Uuid $familyId): array
     {
         $refreshToken = bin2hex(random_bytes(16));
         $accessToken = $this->jwtManager->handle($user);
@@ -103,7 +50,7 @@ class AuthService
             id: Uuid::generate()->value,
             userId: $user->id->value,
             tokenHash: hash('sha256', $refreshToken),
-            familyId: $familyId,
+            familyId: $familyId->value,
             isRevoked: false,
         );
 
@@ -127,10 +74,7 @@ class AuthService
         $now = new \DateTime();
 
         if ($now->diff($expiresAt)->days >= 30) {
-            $this->tokenStorage->update(
-                id: $tokenData['id'],
-                isRevoked: true,
-            );
+            $this->revokeRefreshToken($tokenData['id']);
             throw new \Exception("Cрок действия токена окончен", 401);
         }
 
@@ -138,10 +82,7 @@ class AuthService
         if ($tokenData['is_revoked']) {
             $allTokenData = $this->tokenStorage->findAll('family_id', $familyId);
             foreach ($allTokenData as $tokenItem) {
-                 $this->tokenStorage->update(
-                    id: $tokenItem['id'],
-                    isRevoked: true,
-                );
+                $this->revokeRefreshToken($tokenItem['id']);
             }
 
             throw new \Exception('Токен недействителен', 401);
@@ -153,10 +94,7 @@ class AuthService
             throw new \Exception('Пользователь не найден', 404);
         }
 
-        $this->tokenStorage->update(
-            id: $tokenData['id'],
-            isRevoked: true,
-        );
+        $this->revokeRefreshToken($tokenData['id']);
 
         return $this->generateTokens($user, $familyId);
     }
